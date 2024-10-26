e4501e8b82994cf12cc403a2c8d10f6d86094901
tribalfs <tribalfs@gmail.com>
2024-10-26 19:55:25 +0800

    app: QRCodeFragment: migrate to QRImageView

--- sample-app/src/main/java/dev/oneuiproject/oneuiexample/fragment/QRCodeFragment.java
+++ sample-app/src/main/java/dev/oneuiproject/oneuiexample/fragment/QRCodeFragment.java
@@ -15,36 +15,6 @@ import dev.oneuiproject.oneuiexample.base.BaseFragment;
 
 public class QRCodeFragment extends BaseFragment {
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        ImageView qr_image_1 = view.findViewById(R.id.qr_image_1);
-        ImageView qr_image_2 = view.findViewById(R.id.qr_image_2);
-        ImageView qr_image_3 = view.findViewById(R.id.qr_image_3);
-        ImageView qr_image_4 = view.findViewById(R.id.qr_image_4);
-
-        qr_image_1.setImageBitmap(new QREncoder(mContext, "The One UI Sample app has been made to showcase the components from both our oneui-core libraries and oneui-design module.")
-                .setIcon(R.mipmap.ic_launcher)
-                .generate());
-
-        qr_image_2.setImageBitmap(new QREncoder(mContext, "https://github.com/OneUIProject/oneui-design/raw/main/sample-app/release/sample-app-release.apk")
-                .setIcon(dev.oneuiproject.oneui.R.drawable.ic_oui_file_type_apk)
-                .setFGColor(Color.parseColor("#ff6ebe64"), true, true)
-                .generate());
-
-        qr_image_3.setImageBitmap(new QREncoder(mContext, "custom colors and size")
-                .setIcon(dev.oneuiproject.oneui.R.drawable.ic_oui_file_type_txt)
-                .setSize(350)
-                .setBGColor(Color.BLACK)
-                .setFGColor(Color.RED, true, true)
-                .generate());
-
-        qr_image_4.setImageBitmap(new QREncoder(mContext, "without frame and icon")
-                .setFrame(false)
-                .generate());
-
-    }
 
     @Override
     public int getLayoutResId() {
