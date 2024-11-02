70540a889e06a675de2f6c22d4bec5d6d58969dc
tribalfs <tribalfs@gmail.com>
2024-11-02 19:24:42 +0800

    app: ContactsFragment: migrate  to AutoHideIndexScrollView

--- sample-app/src/main/java/dev/oneuiproject/oneuiexample/ui/fragment/contacts/ContactsFragment.kt
+++ sample-app/src/main/java/dev/oneuiproject/oneuiexample/ui/fragment/contacts/ContactsFragment.kt
@@ -1,7 +1,6 @@
 package dev.oneuiproject.oneuiexample.ui.fragment.contacts
 
 import android.content.Context
-import android.content.res.Configuration
 import android.database.MatrixCursor
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
@@ -18,24 +17,22 @@ import androidx.appcompat.view.menu.SeslMenuItem
 import androidx.core.view.MenuProvider
 import androidx.indexscroll.widget.SeslCursorIndexer
 import androidx.indexscroll.widget.SeslIndexScrollView
-import androidx.indexscroll.widget.SeslIndexScrollView.OnIndexBarEventListener
 import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sec.sesl.tester.R
 import dev.oneuiproject.oneui.utils.IndexScrollUtils
+import dev.oneuiproject.oneui.widget.AutoHideIndexScrollView
 import dev.oneuiproject.oneuiexample.data.ContactsRepo
 import dev.oneuiproject.oneuiexample.ui.core.base.BaseFragment
 import dev.oneuiproject.oneuiexample.ui.fragment.contacts.adapter.ContactsAdapter
 
 class ContactsFragment : BaseFragment() {
     private var mCurrentSectionIndex = 0
-    private var mIndexScrollView: SeslIndexScrollView? = null
+    private var mIndexScrollView: AutoHideIndexScrollView? = null
     private var mContactsListRv: RecyclerView? = null
 
     private var mIsTextModeEnabled = false
-    private var mIsIndexBarPressed = false
-    private val mHideIndexBar = Runnable { IndexScrollUtils.animateVisibility(mIndexScrollView!!, false) }
 
     private lateinit var contactsAdapter: ContactsAdapter
 
@@ -85,22 +82,6 @@ class ContactsFragment : BaseFragment() {
         }
     }
 
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-
-        val isRtl = newConfig
-            .layoutDirection == View.LAYOUT_DIRECTION_RTL
-        if (mIndexScrollView != null) {
-            mIndexScrollView!!.setIndexBarGravity(
-                if (isRtl)
-                    SeslIndexScrollView.GRAVITY_INDEX_BAR_LEFT
-                else
-                    SeslIndexScrollView.GRAVITY_INDEX_BAR_RIGHT
-            )
-        }
-    }
-
     override fun getLayoutResId(): Int = R.layout.fragment_contacts
 
     override fun getIconResId(): Int = dev.oneuiproject.oneui.R.drawable.ic_oui_contact_outline
@@ -121,77 +102,25 @@ class ContactsFragment : BaseFragment() {
             seslSetGoToTopEnabled(true)
             seslSetSmoothScrollEnabled(true)
         }
+
+        mIndexScrollView!!.apply {
+            attachToRecyclerView(mContactsListRv)
+            updateIndexer(contactsList.map { it[0].toString() })
+        }
     }
 
-    private fun initIndexScroll() {
-        val isRtl = resources.configuration
-            .layoutDirection == View.LAYOUT_DIRECTION_RTL
-
-        mIndexScrollView!!.setIndexBarGravity(
-            if (isRtl)
-                SeslIndexScrollView.GRAVITY_INDEX_BAR_LEFT
-            else
-                SeslIndexScrollView.GRAVITY_INDEX_BAR_RIGHT
-        )
-
-        val contactsList = ContactsRepo().contactsList
-
+    private fun SeslIndexScrollView.updateIndexer(items: List<String>) {
         val cursor = MatrixCursor(arrayOf("item"))
-        for (item in contactsList) {
+        for (item in items){
             cursor.addRow(arrayOf(item))
         }
-
         cursor.moveToFirst()
-
         val indexer = SeslCursorIndexer(cursor, 0,
-            "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,Б".split(",".toRegex())
-                .dropLastWhile { it.isEmpty() }.toTypedArray(), 0
-        )
+            "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,Б".split(",").toTypedArray(), 0)
         indexer.setGroupItemsCount(1)
         indexer.setMiscItemsCount(3)
-
-        mIndexScrollView!!.setIndexer(indexer)
-        mIndexScrollView!!.setOnIndexBarEventListener(
-            object : OnIndexBarEventListener {
-                override fun onIndexChanged(sectionIndex: Int) {
-                    if (mCurrentSectionIndex != sectionIndex) {
-                        mCurrentSectionIndex = sectionIndex
-                        if (mListView!!.scrollState != RecyclerView.SCROLL_STATE_IDLE) {
-                            mListView!!.stopScroll()
-                        }
-                        (mListView!!.layoutManager as LinearLayoutManager)
-                            .scrollToPositionWithOffset(sectionIndex, 0)
-                    }
-                }
-
-                override fun onPressed(v: Float) {
-                    mIsIndexBarPressed = true
-                    mListView!!.removeCallbacks(mHideIndexBar)
-                }
-
-                override fun onReleased(v: Float) {
-                    mIsIndexBarPressed = false
-                    if (mListView!!.scrollState == RecyclerView.SCROLL_STATE_IDLE) {
-                        mListView!!.postDelayed(mHideIndexBar, 1500)
-                    }
-                }
-            })
-        mIndexScrollView!!.attachToRecyclerView(mListView)
-        mListView!!.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-                if (newState == RecyclerView.SCROLL_STATE_IDLE
-                    && !mIsIndexBarPressed
-                ) {
-                    recyclerView.postDelayed(mHideIndexBar, 1500)
-                } else {
-                    mListView!!.removeCallbacks(mHideIndexBar)
-                    IndexScrollUtils.animateVisibility(mIndexScrollView!!, true)
-                }
-            }
-        })
+        setIndexer(indexer)
     }
-
 
     private inner class ItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
         private val mDivider: Drawable?
