75447a09f2f362dc46f6398f2a64f83bd1c2f1ad
tribalfs <tribalfs@gmail.com>
2024-11-19 14:05:18 +0800

    lib: add `onSingleClick` and `semSetToolTipText` View extension functions
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- lib/src/main/java/dev/oneuiproject/oneui/ktx/View.kt
+++ lib/src/main/java/dev/oneuiproject/oneui/ktx/View.kt
@@ -1,6 +1,7 @@
 package dev.oneuiproject.oneui.ktx
 
 import android.view.View
+import androidx.appcompat.widget.TooltipCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -30,3 +31,29 @@ inline val View.isSoftKeyboardShowing
     get() = ViewCompat.getRootWindowInsets(this)?.isVisible(
         WindowInsetsCompat.Type.ime()
     ) == true
+
+
+/**
+ * Sets a click listener that doesn't allow multiple invocations within a given interval.
+ * @param interval The interval in milliseconds.
+ * @param action The lambda function to perform.
+ */
+@JvmOverloads
+inline fun <T: View>T.onSingleClick(interval: Long = 600, crossinline action: (T) -> Unit) {
+    var lastClickTime = 0L
+    setOnClickListener {
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - lastClickTime > interval) {
+            lastClickTime = currentTime
+            action(this)
+        }
+    }
+}
+
+/**
+ * Sets OneUI style tooltip text to this View.
+ */
+inline fun View.semSetToolTipText(toolTipText: CharSequence?) {
+    TooltipCompat.setTooltipText(this, toolTipText)
+}
+
