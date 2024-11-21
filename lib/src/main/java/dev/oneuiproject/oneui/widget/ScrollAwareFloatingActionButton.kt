8ff1cf3228b0c3cd475cbe033127c817af206921
tribalfs <tribalfs@gmail.com>
2024-11-22 04:59:17 +0800

    lib: ScrollAwareFloatingActionButton: major refactoring
    Notes:
    - The auto-hiding behavior is setup using its `hideOnScroll()` method.
    - This has now the following behavior:
      - Hides when scrolling the RV up until stopped and is untouched or when scrolled down
      - Hides while fast scroller is pressed
      - (Optionally) Hides while AutoHideIndexScrollView is pressed when the optional AutoHideIndexScrollView parameter is provided.
      - If put inside "main_content", automatically translates Y when AppBar is expanded
      to maintain visibility
    
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- lib/src/main/java/dev/oneuiproject/oneui/widget/ScrollAwareFloatingActionButton.kt
+++ lib/src/main/java/dev/oneuiproject/oneui/widget/ScrollAwareFloatingActionButton.kt
@@ -3,79 +3,75 @@ package dev.oneuiproject.oneui.widget
 import android.content.Context
 import android.os.Build
 import android.util.AttributeSet
-import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.indexscroll.widget.SeslIndexScrollView.OnIndexBarEventListener
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.SCROLL_STATE_IDLE
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import dev.oneuiproject.oneui.design.R
+import dev.oneuiproject.oneui.layout.ToolbarLayout
 import kotlinx.coroutines.Runnable
 import java.lang.ref.WeakReference
 
 
+/**
+ * An extension of [FloatingActionButton] designed for integration with
+ * [ToolbarLayout][dev.oneuiproject.oneui.layout.ToolbarLayout]. This specialized FAB
+ * provides functionality to automatically hide itself during scrolling events
+ * within a [RecyclerView] and with [AutoHideIndexScrollView].
+ *
+ * @see hideOnScroll
+ */
 class ScrollAwareFloatingActionButton @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
-) : FloatingActionButton(context, attrs) {
+) : FloatingActionButton(context, attrs), AppBarLayout.OnOffsetChangedListener {
+
+    private enum class ScrollState{
+        SCROLLING_UP, SCROLLING_DOWN, IDLE
+    }
 
     private val showRunnable = Runnable { show() }
 
     private var setVisibility = View.VISIBLE
-    private var _temporaryHide = false
+    private var isTouchingRv = false
+    private var fastScrollerPressed = false
+    private var scrollState: ScrollState = ScrollState.IDLE
 
-    //This FAB might have a longer lifecycle than the RecyclerView.
+    //This FAB might have a longer lifecycle than the RecyclerView
+    // and AutoHideIndexScrollView.
     private var recyclerViewWR: WeakReference<RecyclerView>? = null
-    private var lastRVScrollState = RecyclerView.SCROLL_STATE_IDLE
+    private var indexScrollViewWR: WeakReference<AutoHideIndexScrollView>? = null
 
-    /**
-     * Set to true to temporarily hide the FAB.
-     * This will be helpful for cases like when on search mode or action mode
-     * where this FAB needs to be temporarily hidden.
-     *
-     */
-    var temporaryHide: Boolean
-        get() = _temporaryHide
-        set(value) {
-            _temporaryHide = value
-            if (value) {
-                _temporaryHide = true
-                removeCallbacks(showRunnable)
-                hide()
-            } else {
-                show()
+    private val indexBarEventListener by lazy {
+        object : OnIndexBarEventListener {
+            override fun onIndexChanged(sectionIndex: Int) {}
+
+            override fun onPressed(v: Float) {
+                fastScrollerPressed = true
+                updateState()
+            }
+
+            override fun onReleased(v: Float) {
+                fastScrollerPressed = false
+                updateState()
             }
         }
+    }
 
     /**
      * Shows the button.
      * This method will animate the button show if the view has already been laid out.
-     * This has no effect when [temporaryHide] is true or [setVisibility] is not set to [View.VISIBLE].
+     * This has no effect when [setVisibility] is not set to [View.VISIBLE].
      */
     override fun show() {
-        if (_temporaryHide) {
-            Log.w(TAG, "calling show")
-            return
-        }
         if (setVisibility == VISIBLE) {
             super.show()
         }
-    }
-
-    private val scrollListener by lazy {
-        object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-                lastRVScrollState = newState
-                if (_temporaryHide) return
-                when (newState) {
-                    RecyclerView.SCROLL_STATE_IDLE -> showDelayed()
-                    else -> hide()
-                }
-            }
-        }
-    }
-
-    private fun showDelayed() {
-        removeCallbacks(showRunnable)
-        postDelayed(showRunnable, 1200)
     }
 
     override fun hide() {
@@ -83,13 +79,72 @@ class ScrollAwareFloatingActionButton @JvmOverloads constructor(
         super.hide()
     }
 
+    private fun showDelayed() {
+        removeCallbacks(showRunnable)
+        postDelayed(showRunnable, 1_500)
+    }
+
+    override fun setVisibility(visibility: Int) {
+        this.setVisibility = visibility
+        when (visibility) {
+            GONE, INVISIBLE -> hide()
+            VISIBLE -> updateState()
+        }
+    }
+
+    private fun updateState(){
+        if (setVisibility != VISIBLE || fastScrollerPressed) {
+            hide()
+            return
+        }
+        when (scrollState){
+            ScrollState.SCROLLING_UP -> {
+                hide()
+            }
+            ScrollState.SCROLLING_DOWN -> {
+                show()
+            }
+            ScrollState.IDLE -> {
+                if (isTouchingRv) return
+                showDelayed()
+            }
+        }
+    }
+
+    private val scrollListener by lazy {
+        object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (dy > SCROLL_DELTA) {
+                    scrollState = ScrollState.SCROLLING_UP
+                    updateState()
+                } else if (dy < -SCROLL_DELTA){
+                    scrollState = ScrollState.SCROLLING_DOWN
+                    updateState()
+                }
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == SCROLL_STATE_IDLE) {
+                    scrollState = ScrollState.IDLE
+                    updateState()
+                }
+            }
+        }
+    }
+
+
     /**
      * Hides this FAB if the provided [RecyclerView] is scrolling
      *
      * @param recyclerView
+     * @param indexScrollView (Optional)
      */
-    fun hideOnScroll(recyclerView: RecyclerView) {
+    @JvmOverloads
+    fun hideOnScroll(recyclerView: RecyclerView, indexScrollView: AutoHideIndexScrollView? = null) {
         recyclerViewWR = WeakReference(recyclerView)
+        indexScrollViewWR = WeakReference(indexScrollView)
         if (isAttachedToWindow) {
             onAttachedToWindowInternal()
         }
@@ -100,66 +155,112 @@ class ScrollAwareFloatingActionButton @JvmOverloads constructor(
 
         recyclerView.removeOnScrollListener(scrollListener)
         recyclerView.addOnScrollListener(scrollListener)
+        recyclerView.doOnTouchEvent{
+            when (it.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    isTouchingRv = true
+                    updateState()
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    isTouchingRv = false
+                    updateState()
+                }
+            }
+        }
+
+        indexScrollViewWR?.get()?.addOnIndexEventListener(indexBarEventListener)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             recyclerView.seslSetFastScrollerEventListener(
                 object : RecyclerView.SeslFastScrollerEventListener {
                     override fun onPressed(scrollY: Float) {
-                        if (!_temporaryHide) {
-                            removeCallbacks(showRunnable)
-                            hide()
-                        }
+                        fastScrollerPressed = true
+                        updateState()
                     }
 
                     override fun onReleased(scrollY: Float) {
-                        if (!_temporaryHide) {
-                            showDelayed()
-                        }
+                        fastScrollerPressed = false
+                        updateState()
                     }
-
                 }
             )
         }
-        if (lastRVScrollState == RecyclerView.SCROLL_STATE_IDLE) showDelayed()
+        updateState()
     }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
         onAttachedToWindowInternal()
+        offsetYIfInsideMainContainer(true)
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-        hide()
         recyclerViewWR?.get()?.apply {
             removeOnScrollListener(scrollListener)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 seslSetFastScrollerEventListener(null)
             }
+            setOnTouchListener(null)
         }
+        indexScrollViewWR?.get()?.removeOnIndexEventListener(indexBarEventListener)
+        offsetYIfInsideMainContainer(false)
     }
 
-
-    override fun setVisibility(visibility: Int) {
-        this.setVisibility = visibility
-        when (visibility) {
-            GONE, INVISIBLE -> {
-                if (isOrWillBeHidden) return
-                removeCallbacks(showRunnable)
-                super.setVisibility(visibility)
-            }
-
-            VISIBLE -> {
-                if (_temporaryHide && !isOrWillBeHidden) {
-                    super.setVisibility(GONE)
-                    return
-                }
+    private fun offsetYIfInsideMainContainer(register: Boolean){
+        if (!layoutLocationInfo.isInsideTBLMainContainer) return
+        layoutLocationInfo.tblParent!!.appBarLayout.apply {
+            if (register){
+                addOnOffsetChangedListener(this@ScrollAwareFloatingActionButton)
+            } else {
+                removeOnOffsetChangedListener(this@ScrollAwareFloatingActionButton)
             }
         }
     }
 
+    private val layoutLocationInfo: InternalLayoutInfo by lazy {
+        var isInsideTBLMainContainer = false
+        var toolbarLayout: ToolbarLayout? = null
+        var currentParent = parent
+        while (currentParent is ViewGroup) {
+            if (currentParent.id == R.id.toolbarlayout_main_container) {
+                isInsideTBLMainContainer = true
+                if (toolbarLayout != null) break
+            }
+            if (currentParent is ToolbarLayout) {
+                toolbarLayout = currentParent
+                if (isInsideTBLMainContainer) break
+            }
+            currentParent = currentParent.parent
+        }
+        InternalLayoutInfo(isInsideTBLMainContainer, toolbarLayout)
+    }
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+        if (!isVisible) return
+        translationY = (verticalOffset + appBarLayout.totalScrollRange).toFloat() * -1f
+    }
+
+    private data class InternalLayoutInfo(
+        val isInsideTBLMainContainer: Boolean,
+        val tblParent: ToolbarLayout?
+    )
 
     companion object {
         private const val TAG = "ScrollAwareFAB"
+        private const val SCROLL_DELTA = 4f
     }
+}
 
+private fun RecyclerView.doOnTouchEvent(action: (MotionEvent) -> Unit ) {
+    val actionWR = WeakReference(action)
+    addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
+        override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+            actionWR.get()?.invoke(e)
+            return false
+        }
+
+        override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
+        override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
+    })
 }
