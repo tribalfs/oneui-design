ffa8bb4577641b500798f69c088ce1bc43fe162e
tribalfs <tribalfs@gmail.com>
2024-11-30 05:30:28 +0800

    lib: Refactor SearchHighlighter - now uses bold styling for highlighted text
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- lib/src/main/java/dev/oneuiproject/oneui/utils/SearchHighlighter.kt
+++ lib/src/main/java/dev/oneuiproject/oneui/utils/SearchHighlighter.kt
@@ -2,66 +2,112 @@
 
 package dev.oneuiproject.oneui.utils
 
+import android.content.Context
 import android.graphics.Color
+import android.text.Spannable
 import android.text.SpannableString
 import android.text.SpannableStringBuilder
 import android.text.Spanned.SPAN_MARK_MARK
 import android.text.style.ForegroundColorSpan
+import android.text.style.TextAppearanceSpan
 import androidx.annotation.ColorInt
 import androidx.core.text.clearSpans
+import dev.oneuiproject.oneui.design.R
 import java.util.StringTokenizer
 
-class SearchHighlighter {
+/**
+ * A Util class intended for highlighting text based on a search string.
+ *
+ * @param context Provides the ability to set highlight color to context theme's colorPrimaryDark.
+ * @param highlightColor Set a custom color to override default highlight color
+ *
+ * if neither context nor custom highlightColor is provided, the color will be set to #2196F3.
+ *
+ */
+class SearchHighlighter @JvmOverloads constructor(@JvmField val context: Context? = null,
+                        @JvmField @ColorInt var highlightColor: Int = -1) {
 
-    @JvmField
-    @ColorInt
-    var highlightColor = Color.parseColor("#2196F3")
-
-    inline operator fun invoke(
-        spannableStringBuilder: SpannableStringBuilder,
-        query: String,
-    ): SpannableStringBuilder {
-        if (query.isEmpty()) return spannableStringBuilder
-        return invoke(spannableStringBuilder, StringTokenizer(query))
-    }
-
-    inline operator fun invoke(
-        spannableStringBuilder: SpannableStringBuilder,
-        searchTokens: StringTokenizer,
-    ): SpannableStringBuilder {
-        spannableStringBuilder.clearSpans()
-        while (searchTokens.hasMoreTokens()) {
-            val nextToken = searchTokens.nextToken()
-            var remainingString = spannableStringBuilder.toString()
-            var offsetEnd = 0
-            do {
-                val index = remainingString.indexOf(nextToken, ignoreCase = true)
-                if (index < 0) break
-                val length = index + nextToken.length
-                val offsetStart = index + offsetEnd
-                offsetEnd += length
-                spannableStringBuilder.setSpan(ForegroundColorSpan(highlightColor), offsetStart, offsetEnd, SPAN_MARK_MARK)
-                remainingString = remainingString.substring(length)
-                if (remainingString.indexOf(nextToken, ignoreCase = true) != -1) {
-                    break
-                }
-            } while (offsetEnd < MAX_OFFSET)
+    init {
+        if (context == null && highlightColor == -1) {
+            highlightColor =  Color.parseColor("#2196F3")
         }
-        return spannableStringBuilder
     }
 
-    inline operator fun invoke(
-        spannableString: SpannableString,
-        query: String,
+    /**
+     * Highlights the occurrences of the query tokens in the provided [SpannableString].
+     *
+     * @param spannable The [SpannableString] to apply highlighting to.
+     * @param searchString The search string containing terms to highlight.
+     * @return The [SpannableString] with highlights applied.
+     */
+    operator fun invoke(
+        spannable: SpannableString,
+        searchString: String
     ): SpannableString {
-        if (query.isEmpty()) return spannableString
-        return invoke(spannableString, StringTokenizer(query))
+        if (searchString.isEmpty()) return spannable
+        applyHighlighting(spannable, StringTokenizer(searchString))
+        return spannable
     }
 
-    inline operator fun invoke(
-        spannableString: SpannableString,
-        searchTokens: StringTokenizer,
+    /**
+     * Highlights the occurrences of the query tokens in the provided [SpannableString].
+     *
+     * @param spannable The [SpannableStringBuilder] to apply highlighting to.
+     * @param searchString The search string containing terms to highlight.
+     * @return The [SpannableString] with highlights applied.
+     */
+    operator fun invoke(
+        spannable: SpannableStringBuilder,
+        searchString: String
+    ): SpannableStringBuilder {
+        if (searchString.isEmpty()) return spannable
+        applyHighlighting(spannable, StringTokenizer(searchString))
+        return spannable
+    }
+
+    /**
+     * Highlights the occurrences of the query tokens in the provided text.
+     *
+     * @param text The [CharSequence] to search and highlight.
+     * @param searchString The search string containing terms to highlight.
+     * @return A [SpannableString] with highlights applied.
+     */
+    operator fun invoke(
+        text: CharSequence,
+        searchString: String
     ): SpannableString {
+        val spannable = SpannableString(text)
+        if (searchString.isEmpty()) return spannable
+        applyHighlighting(spannable, StringTokenizer(searchString))
+        return spannable
+    }
+
+    /**
+     * Highlights the occurrences of the query tokens in the provided text.
+     *
+     * @param text The [CharSequence] to search and highlight.
+     * @param searchTokens The search StringTokenizer containing terms to highlight.
+     * @return A [SpannableString] with highlights applied.
+     */
+    operator fun invoke(
+        text: CharSequence,
+        searchTokens: StringTokenizer
+    ): SpannableString {
+        val spannable = SpannableString(text)
+        applyHighlighting(spannable, searchTokens)
+        return spannable
+    }
+
+    /**
+     * Applies highlighting to the provided [Spannable] based on the search query.
+     *
+     * @param spannableString The [Spannable] to apply highlighting to.
+     * @param searchTokens The StringTokenizer containing terms to highlight.
+     */
+    private fun applyHighlighting(
+        spannableString: Spannable,
+        searchTokens: StringTokenizer
+    ) {
         spannableString.clearSpans()
         while (searchTokens.hasMoreTokens()) {
             val nextToken = searchTokens.nextToken()
@@ -73,25 +119,27 @@ class SearchHighlighter {
                 val length = index + nextToken.length
                 val offsetStart = index + offsetEnd
                 offsetEnd += length
-                spannableString.setSpan(ForegroundColorSpan(highlightColor), offsetStart, offsetEnd, SPAN_MARK_MARK)
+
+                context?.let {
+                    spannableString.setSpan(
+                        TextAppearanceSpan(it, R.style.OneUI_SearchHighlightedTextAppearance), offsetStart,
+                        offsetEnd, SPAN_MARK_MARK)
+                }
+
+                if (highlightColor != -1) {
+                    spannableString.setSpan(ForegroundColorSpan(highlightColor), offsetStart, offsetEnd, SPAN_MARK_MARK)
+                }
+
                 remainingString = remainingString.substring(length)
                 if (remainingString.indexOf(nextToken, ignoreCase = true) != -1) {
                     break
                 }
             } while (offsetEnd < MAX_OFFSET)
         }
-        return spannableString
-    }
-
-    inline operator fun invoke(
-        textToSearch: CharSequence,
-        query:  String,
-    ): SpannableString {
-        return invoke(SpannableString(textToSearch), StringTokenizer(query))
     }
 
     companion object{
-         const val MAX_OFFSET = 200
+        const val MAX_OFFSET = 200
     }
 
 }
