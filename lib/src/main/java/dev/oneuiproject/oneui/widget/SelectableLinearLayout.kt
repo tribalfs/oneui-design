c0f68142401cc8cfbbeca16a5a1c9f9b80c61eaf
tribalfs <tribalfs@gmail.com>
2025-01-03 17:37:21 +0800

    lib: refactor SelectableLinearLayout - overlayCircle now supports custom corner radius
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- lib/src/main/java/dev/oneuiproject/oneui/widget/SelectableLinearLayout.kt
+++ lib/src/main/java/dev/oneuiproject/oneui/widget/SelectableLinearLayout.kt
@@ -6,16 +6,17 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.util.AttributeSet
+import android.util.Log
 import android.view.Gravity
 import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.LinearLayout
-import androidx.appcompat.content.res.AppCompatResources
+import androidx.annotation.Px
 import androidx.core.content.res.use
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
-import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import dev.oneuiproject.oneui.design.R
+import dev.oneuiproject.oneui.widget.internal.SelectableAnimatedDrawable
 
 /**
  * A custom [LinearLayout] designed to be used as a selectable item within a
@@ -32,38 +33,26 @@ class SelectableLinearLayout @JvmOverloads constructor(
     defStyleRes: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
 
-    private var mDisabledAlpha: Float = 0.4f
-    private var mCheckBox: CheckBox? = null
     private lateinit var selectedHighlightColor: ColorDrawable
-    private var mCheckMode: Int = 0
-    private lateinit var mColorBackground: ColorDrawable
+    private var checkMode: Int = CHECK_MODE_CHECKBOX
 
-    private var checkDrawable: AnimatedVectorDrawableCompat? = null
-    private var uncheckDrawable: AnimatedVectorDrawableCompat? = null
+    //For CHECK_MODE_CHECKBOX
+    private var mCheckBox: CheckBox? = null
+
+    //For CHECK_MODE_OVERLAY
+    private var checkDrawable: SelectableAnimatedDrawable? = null
     private var imageTargetId: Int? = null
     private var imageTarget: ImageView? = null
 
     init {
-        context.obtainStyledAttributes(
-            intArrayOf(
-                android.R.attr.colorBackground,
-                android.R.attr.disabledAlpha
-            ))
-            .use{
-                mColorBackground = ColorDrawable(it.getColor(0, 0))
-                mDisabledAlpha = it.getFloat(1, .4f)
-            }
-
         context.obtainStyledAttributes(attrs, R.styleable.SelectableLinearLayout).use {
             val color = it.getColor(R.styleable.SelectableLinearLayout_selectedHighlightColor,
                 Color.parseColor("#08000000"))
             selectedHighlightColor = ColorDrawable(color)
 
-            if (Build.VERSION.SDK_INT >= 23) {
-                mCheckMode = it.getInt(R.styleable.SelectableLinearLayout_checkMode, 0)
-            }
+            checkMode = it.getInt(R.styleable.SelectableLinearLayout_checkMode, CHECK_MODE_CHECKBOX)
 
-            when (mCheckMode){
+            when (checkMode){
                 0 ->{
                     val spacing = it.getDimensionPixelSize(
                         R.styleable.SelectableLinearLayout_checkableButtonSpacing, 14)
@@ -80,12 +69,12 @@ class SelectableLinearLayout @JvmOverloads constructor(
                     }
                     addView(mCheckBox, 0)
                 }
-                else -> {
-                    if (mCheckMode == 1) {
-                        checkDrawable = AnimatedVectorDrawableCompat.create(context,
-                            R.drawable.ic_oui_check_selected) as AnimatedVectorDrawableCompat
-                        uncheckDrawable = AnimatedVectorDrawableCompat.create(context,
-                            R.drawable.ic_oui_check_unselected) as AnimatedVectorDrawableCompat
+                1 -> {
+                    checkDrawable = SelectableAnimatedDrawable.create(context, R.drawable.ic_oui_check_animated_selector, context.theme)
+                    if (it.hasValue(R.styleable.SelectableLinearLayout_cornerRadius)){
+                        checkDrawable!!.setCornerRadius(
+                            it.getDimension(R.styleable.SelectableLinearLayout_cornerRadius, 0f)
+                        )
                     }
                     imageTargetId = it.getResourceId(R.styleable.SelectableLinearLayout_targetImage, 0)
                 }
@@ -95,12 +84,13 @@ class SelectableLinearLayout @JvmOverloads constructor(
 
     override fun onFinishInflate() {
         super.onFinishInflate()
-        if (mCheckMode == 1) {
+        if (checkMode == CHECK_MODE_OVERLAY) {
             imageTarget = findViewById(imageTargetId!!)!!
             imageTargetId = null
-            if (isInEditMode) {
-                @Suppress("NewApi")
+            if (Build.VERSION.SDK_INT >= 23) {
                 imageTarget!!.foreground = checkDrawable
+            }else{
+                imageTarget!!.background = checkDrawable
             }
         }
     }
@@ -112,61 +102,48 @@ class SelectableLinearLayout @JvmOverloads constructor(
         set(value) {
             if (field == value) return
             field = value
-            when (mCheckMode){
-                0 -> {
-                    mCheckBox!!.isVisible = value
-                }
+            when (checkMode){
+                0 -> mCheckBox!!.isVisible = value
                 else -> Unit
             }
-
         }
-
 
     /**
      * Use to toggle selected state indicators on this view
      * @see setSelectedAnimate
      */
     override fun setSelected(isSelected: Boolean) {
-        when (mCheckMode){
-            0 ->  mCheckBox!!.isChecked = isSelected
-            1 -> {
-                @Suppress("NewApi")
-                imageTarget!!.foreground = if (isSelected) checkDrawable else null
-            }
-
-        }
-        background = if (isSelected) selectedHighlightColor else null
-    }
+        setSelectedAnimate(isSelected)
+        checkDrawable?.jumpToCurrentState()
+     }
 
     /**
      * Same as [setSelected] but
      * will animate the check drawable when `checkMode` is set to `overlayCircle`.
      */
     fun setSelectedAnimate(isSelected: Boolean){
-        when (mCheckMode){
-            0 -> {
-                mCheckBox!!.isChecked = isSelected
-            }
+        when (checkMode){
+            0 -> mCheckBox!!.isChecked = isSelected
             1 -> {
-                @Suppress("NewApi")
-                if (isSelected) {
-                    imageTarget!!.apply icon@ {
-                        if (this@icon.foreground != checkDrawable) {
-                            this@icon.foreground = checkDrawable
-                            checkDrawable!!.start()
-                        }
-                    }
-                }else{
-                    imageTarget!!.apply icon@ {
-                        if (this@icon.foreground == checkDrawable) {
-                            this@icon.foreground = uncheckDrawable
-                            uncheckDrawable!!.start()
-                        }
-                    }
+                if (Build.VERSION.SDK_INT < 23) {
+                    imageTarget!!.imageAlpha = if (!isSelected) 255 else 0
                 }
+                imageTarget!!.isSelected = isSelected
             }
         }
         background = if (isSelected) selectedHighlightColor else null
+    }
 
+    fun setOverlayCornerRadius(@Px radius: Float){
+        when (checkMode){
+            1 -> checkDrawable!!.setCornerRadius(radius)
+            else -> Log.w(TAG, "setOverlayCornerRadius only applies to overlayCircle mode.")
+        }
+    }
+
+    companion object{
+        private const val TAG = "SelectableLinearLayout"
+        private const val CHECK_MODE_CHECKBOX = 0
+        private const val CHECK_MODE_OVERLAY = 1
     }
 }
---
+++ lib/src/main/java/dev/oneuiproject/oneui/widget/internal/SelectableAnimatedDrawable.kt
@@ -0,0 +1,154 @@
+package dev.oneuiproject.oneui.widget.internal
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.content.res.Resources
+import android.content.res.Resources.Theme
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.ColorFilter
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.util.Log
+import android.util.Xml
+import android.widget.ImageView
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+import androidx.annotation.Px
+import androidx.annotation.RestrictTo
+import androidx.appcompat.graphics.drawable.AnimatedStateListDrawableCompat
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
+import dev.oneuiproject.oneui.ktx.getThemeAttributeValue
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserException
+import java.io.IOException
+
+@RestrictTo(RestrictTo.Scope.LIBRARY)
+class SelectableAnimatedDrawable : AnimatedStateListDrawableCompat() {
+
+    @Px
+    private var radius = -1f
+    @ColorInt
+    private var selectedColor: Int = -1
+    private var backgroundPaint: Paint? = null
+
+    init {
+        backgroundPaint = Paint().apply {
+            color = Color.TRANSPARENT
+            style = Paint.Style.FILL
+            isAntiAlias = true
+        }
+    }
+
+    override fun inflate(
+        context: Context,
+        resources: Resources,
+        parser: XmlPullParser,
+        attrs: AttributeSet,
+        theme: Resources.Theme?
+    ) {
+        selectedColor = ColorUtils.setAlphaComponent(ContextCompat.getColor(context,
+            context.getThemeAttributeValue(androidx.appcompat.R.attr.colorPrimaryDark)!!.resourceId), (.8f * 255).toInt())
+        super.inflate(context, resources, parser, attrs, theme)
+    }
+
+    private val shapeBounds = RectF()
+
+    override fun draw(canvas: Canvas) {
+        (callback as? ImageView)?.drawable?.let { shapeBounds.set(it.bounds) }
+        val radiusPx = if (radius == -1f) shapeBounds.height()/2f else radius
+        canvas.drawRoundRect(shapeBounds, radiusPx, radiusPx, backgroundPaint!!)
+        super.draw(canvas)
+    }
+
+    override fun setAlpha(alpha: Int) {
+        super.setAlpha(alpha)
+        backgroundPaint?.alpha = alpha
+    }
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+        super.setColorFilter(colorFilter)
+        backgroundPaint?.setColorFilter(colorFilter)
+    }
+
+    private var backgroundAnimator: ValueAnimator? = null
+
+    override fun onStateChange(stateSet: IntArray): Boolean {
+        return super.onStateChange(stateSet).also {
+            if (!it) return@also
+            backgroundAnimator?.cancel()
+            backgroundPaint?.apply {
+                if (stateSet.contains(android.R.attr.state_selected)) {
+                    if (color == selectedColor) return@also
+                    color = selectedColor
+                } else {
+                    if (color == Color.TRANSPARENT) return@also
+                    backgroundAnimator = ValueAnimator.ofArgb(selectedColor, Color.TRANSPARENT).apply {
+                        duration = 250
+                        startDelay = 100
+                        addUpdateListener { animator ->
+                            color = animator.animatedValue as Int
+                            invalidateSelf()
+                        }
+                        start()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun jumpToCurrentState() {
+        super.jumpToCurrentState()
+        backgroundAnimator?.cancel()
+        backgroundPaint?.apply {
+            if (state.contains(android.R.attr.state_selected)) {
+                if (color == selectedColor) return
+                color = selectedColor
+            } else {
+                if (color == Color.TRANSPARENT) return
+                color = Color.TRANSPARENT
+            }
+        }
+    }
+
+    fun setCornerRadius(@Px radius: Float){
+        this.radius = radius
+        invalidateSelf()
+    }
+
+    companion object{
+        private val LOG_TAG: String = SelectableAnimatedDrawable::class.java.simpleName
+
+        fun create(
+            context: Context,
+            @DrawableRes resId: Int,
+            theme: Theme?
+        ): SelectableAnimatedDrawable? {
+            try {
+                val res = context.resources
+                val parser: XmlPullParser = res.getXml(@Suppress("ResourceType") resId)
+                val attrs = Xml.asAttributeSet(parser)
+
+                var type: Int
+                while ((parser.next().also { type = it }) != XmlPullParser.START_TAG
+                    && type != XmlPullParser.END_DOCUMENT) {/*Empty loop*/ }
+                if (type != XmlPullParser.START_TAG) throw XmlPullParserException("No start tag found")
+
+                parser.name.let {
+                    if (it != "animated-selector") throw XmlPullParserException("${parser.positionDescription}:" +
+                            " invalid animated-selector tag $it.") }
+
+                return SelectableAnimatedDrawable().apply {
+                    inflate(context, res, parser, attrs, theme)
+                }
+            } catch (e: XmlPullParserException) {
+                Log.e(LOG_TAG, "parser error", e)
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "parser error", e)
+            }
+            return null
+        }
+    }
+}
