1e1c62db00cf5aa522d3696aff34c9d1b8952ea3
tribalfs <tribalfs@gmail.com>
2025-01-16 20:13:01 +0800

    lib: *DrawerLayout: minor badge updating adjs.
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- lib/src/main/java/dev/oneuiproject/oneui/layout/internal/util/DrawerLayoutUtils.kt
+++ lib/src/main/java/dev/oneuiproject/oneui/layout/internal/util/DrawerLayoutUtils.kt
@@ -4,19 +4,25 @@ package dev.oneuiproject.oneui.layout.internal.util
 
 import android.view.ViewGroup.MarginLayoutParams
 import android.widget.TextView
-import androidx.appcompat.R
+import androidx.appcompat.R as appcompatR
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
+import dev.oneuiproject.oneui.design.R
 import dev.oneuiproject.oneui.ktx.dpToPx
 import dev.oneuiproject.oneui.layout.Badge
 import dev.oneuiproject.oneui.layout.DrawerLayout.DrawerState
-import dev.oneuiproject.oneui.utils.badgeCountToText
 
 object DrawerLayoutUtils {
 
     internal fun TextView.updateBadgeView(badge: Badge, marginOffset: Int = 0){
+
+        val badgeText = badge.toBadgeText()
+        if (badgeText == getTag(R.id.tag_badge_text)) return
+        setTag(R.id.tag_badge_text, badgeText)
+        text = badgeText
+
         val res = resources
         val badgeHeight: Int
         val badgeWidth: Int
@@ -25,26 +31,25 @@ object DrawerLayoutUtils {
         when(badge){
             Badge.NONE -> {
                 isGone = true
-                text = null
                 return
             }
             Badge.DOT  -> {
-                text = null
-                background = AppCompatResources.getDrawable(context, R.drawable.sesl_dot_badge)
-                badgeHeight = res.getDimensionPixelSize(R.dimen.sesl_menu_item_badge_size)
+                background = AppCompatResources.getDrawable(context, appcompatR.drawable.sesl_dot_badge)
+                badgeHeight = res.getDimensionPixelSize(appcompatR.dimen.sesl_menu_item_badge_size)
                 badgeWidth = badgeHeight
                 badgeMargin = (8 + marginOffset).dpToPx(res)
             }
             is Badge.NUMERIC -> {
-                text = badge.count.badgeCountToText()!!
-                background = AppCompatResources.getDrawable(context, R.drawable.sesl_noti_badge)
-                val defaultWidth = res.getDimensionPixelSize(R.dimen.sesl_badge_default_width)
-                val additionalWidth = res.getDimensionPixelSize(R.dimen.sesl_badge_additional_width)
+                background = AppCompatResources.getDrawable(context, appcompatR.drawable.sesl_noti_badge)
+                val defaultWidth = res.getDimensionPixelSize(appcompatR.dimen.sesl_badge_default_width)
+                val additionalWidth = res.getDimensionPixelSize(appcompatR.dimen.sesl_badge_additional_width)
                 badgeHeight = defaultWidth + additionalWidth
                 badgeWidth = defaultWidth + additionalWidth * text.length
                 badgeMargin = (6 + marginOffset).dpToPx(res)
             }
         }
+
+
         updateLayoutParams<MarginLayoutParams> {
             width = badgeWidth
             height = badgeHeight
