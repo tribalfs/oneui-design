ea233f001e1a1b30828c7fcc8d98ad7b533d2b9e
tribalfs <tribalfs@gmail.com>
2025-02-25 18:50:36 +0800

    lib: cleanup leftovers
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- lib/src/main/java/dev/oneuiproject/oneui/ktx/List.kt
+++ lib/src/main/java/dev/oneuiproject/oneui/ktx/List.kt
@@ -1,10 +1,6 @@
 @file:JvmName("ListUtil")
 package dev.oneuiproject.oneui.ktx
 
-import android.os.Build
-import android.os.LocaleList
-import androidx.annotation.RequiresApi
-
 inline fun <T> List<T>.findWithIndex(predicate: (T) -> Boolean, onFound: (index: Int, item: T) -> Unit): Boolean {
     for (index in indices) {
         val item = this[index]
