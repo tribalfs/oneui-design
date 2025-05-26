137c13322ed37af6ab35e3092f0f5106569e84c6
tribalfs <tribalfs@gmail.com>
2025-05-26 19:33:17 +0800

    lib: Navigation: add support for programmatic nav graph + refactor handling for activity and floating destinations
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- lib/src/main/java/dev/oneuiproject/oneui/navigation/Navigation.kt
+++ lib/src/main/java/dev/oneuiproject/oneui/navigation/Navigation.kt
@@ -1,3 +1,4 @@
+
 @file:Suppress("NOTHING_TO_INLINE")
 @file:JvmName("DrawerNavigation")
 
@@ -6,17 +7,26 @@ package dev.oneuiproject.oneui.navigation
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.annotation.FloatRange
+import androidx.navigation.ActivityNavigator
 import androidx.navigation.FloatingWindow
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph
 import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
+import androidx.navigation.createGraph
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import dev.oneuiproject.oneui.layout.NavDrawerLayout
 import dev.oneuiproject.oneui.navigation.delegates.OnBackAppBarHandler
 import dev.oneuiproject.oneui.navigation.widget.DrawerNavigationView
+import kotlin.reflect.KClass
 
+/**
+ * Sets up navigation for [NavDrawerLayout] with a [DrawerNavigationView] and [NavHostFragment].
+ * Supports both XML and programmatically created navigation graphs (NavGraphBuilder DSL).
+ */
 @JvmName("setup")
 @JvmOverloads
 fun NavDrawerLayout.setupNavigation(
@@ -40,12 +50,16 @@ fun NavDrawerLayout.setupNavigation(
             return@setNavigationItemSelectedListener false
         }
 
-        onNavDestinationSelected(item, navController, startDestination.id).also { handled ->
+        onNavDestinationSelected(item, navController).also { handled ->
             if (handled) {
-                if (isActionMode) endActionMode()
-                if (isSearchMode) endSearchMode()
-                setDrawerOpen(false, animate = true, ignoreOnNavRailMode = true)
-                closeNavRailOnBack = item.itemId == startDestination.id
+                val destinationNode = navController.currentDestination!!.parent!!.findNode(item.itemId)
+                if (destinationNode !is FloatingWindow
+                    && destinationNode !is ActivityNavigator.Destination) {
+                    if (isActionMode) endActionMode()
+                    if (isSearchMode) endSearchMode()
+                    setDrawerOpen(false, animate = true, ignoreOnNavRailMode = true)
+                    closeNavRailOnBack = item.itemId == startDestination.id
+                }
             } else {
                 navController.currentDestination?.let {
                     drawerNavigationView.updateSelectedItem(it)
@@ -69,20 +83,58 @@ fun NavDrawerLayout.setupNavigation(
     )
 }
 
+/**
+ * Overload for programmatic navigation graph creation using NavGraphBuilder DSL.
+ *
+ * Example usage:
+ * ```
+ * navDrawerLayout.setupNavigation(
+ *     drawerNavigationView,
+ *     navHostFragment,
+ *     startDestination = "widgets_dest"
+ * ) {
+ *     mainNavGraph()
+ * }
+ * ```
+ */
+@JvmName("setupDsl")
+@JvmOverloads
+fun NavDrawerLayout.setupNavigation(
+    drawerNavigationView: DrawerNavigationView,
+    navHostFragment: NavHostFragment,
+    startDestination: KClass<*>,
+    route: KClass<*>? = null,
+    configuration: AppBarConfiguration = AppBarConfiguration(drawerNavigationView.getDrawerMenu(), this),
+    @FloatRange(0.0, 1.0)
+    toolbarBackThreshold: Float = 0.75f,
+    builder: NavGraphBuilder.() -> Unit
+) {
+    val navController = navHostFragment.navController
+    val graph: NavGraph = navController.createGraph(
+        startDestination = startDestination,
+        route = route,
+        builder = builder
+    )
+    navController.graph = graph
+    setupNavigation(drawerNavigationView, navHostFragment, configuration, toolbarBackThreshold)
+}
 
 private inline fun onNavDestinationSelected(
     item: MenuItem,
-    navController: NavController,
-    startDestinationId: Int
+    navController: NavController
 ): Boolean {
-    val options = NavOptions.Builder()
+    val builder = NavOptions.Builder()
         .setLaunchSingleTop(true)
         .setRestoreState(true)
-        .setPopUpTo(startDestinationId, inclusive = false, saveState = true)
-        .build()
+
+    val destinationNode = navController.currentDestination!!.parent!!.findNode(item.itemId)
+    if (destinationNode !is ActivityNavigator.Destination
+        && destinationNode !is FloatingWindow) {
+        builder.setPopUpTo(navController.graph.findStartDestination().id, inclusive = false)
+    }
 
     return try {
-        navController.navigate(item.itemId, null, options)
+        navController.navigate(item.itemId, null, builder.build())
         true
     } catch (_: IllegalArgumentException) {
         false
