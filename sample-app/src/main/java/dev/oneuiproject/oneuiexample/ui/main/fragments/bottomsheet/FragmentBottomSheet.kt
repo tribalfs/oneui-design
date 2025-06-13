1437493a23e1b7d17e3a84e493f130c054f5bc08
tribalfs <tribalfs@gmail.com>
2025-06-13 20:12:02 +0800

    app: use SemBottomSheetDialogFragment
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- sample-app/src/main/java/dev/oneuiproject/oneuiexample/ui/main/fragments/bottomsheet/FragmentBottomSheet.kt
+++ sample-app/src/main/java/dev/oneuiproject/oneuiexample/ui/main/fragments/bottomsheet/FragmentBottomSheet.kt
@@ -1,34 +1,6 @@
 package dev.oneuiproject.oneuiexample.ui.main.fragments.bottomsheet
 
-import android.app.Dialog
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.sec.sesl.tester.R
-import com.sec.sesl.tester.databinding.FragmentBottomsheetBinding
-import dev.oneuiproject.oneuiexample.ui.main.core.util.autoCleared
+import dev.oneuiproject.oneui.app.SemBottomSheetDialogFragment
 
-class FragmentBottomSheet : BottomSheetDialogFragment(R.layout.fragment_bottomsheet) {
-
-    private val binding by autoCleared { FragmentBottomsheetBinding::bind }
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return (super.onCreateDialog(savedInstanceState) as BottomSheetDialog).apply {
-            behavior.skipCollapsed = true
-            setOnShowListener {
-                behavior.state = BottomSheetBehavior.STATE_EXPANDED
-            }
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View = FragmentBottomsheetBinding.inflate(inflater, container, false).root
-
-}
+class FragmentBottomSheet : SemBottomSheetDialogFragment(R.layout.fragment_bottomsheet)
