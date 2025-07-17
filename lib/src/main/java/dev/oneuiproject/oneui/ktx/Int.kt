dc2d5a1fd234103de8eb5efe0aff56e8d9eaa32f
tribalfs <tribalfs@gmail.com>
2025-07-18 06:32:15 +0800

    lib: add callsInPlace contracts to some KTX
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- lib/src/main/java/dev/oneuiproject/oneui/ktx/Int.kt
+++ lib/src/main/java/dev/oneuiproject/oneui/ktx/Int.kt
@@ -6,6 +6,9 @@ import android.content.res.Resources
 import android.util.DisplayMetrics
 import androidx.annotation.Dimension
 import androidx.annotation.Px
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 @Px
 inline fun Int.dpToPx(densityScale: Float): Int = (this * densityScale).toInt()
@@ -22,6 +25,18 @@ inline fun Int.dpToPx(resources: Resources): Int = toFloat().dpToPx(resources)
 @Dimension(unit = Dimension.DP)
 inline fun Int.pxToDp(resources: Resources): Float = (this / resources.displayMetrics.density)
 
-inline fun Int.ifNegativeOrZero(block: () -> Int): Int = if (this <= 0) block() else this
+@OptIn(ExperimentalContracts::class)
+inline fun Int.ifNegativeOrZero(block: () -> Int): Int {
+    contract {
+        callsInPlace(block, InvocationKind.AT_MOST_ONCE)
+    }
+    return if (this <= 0) block() else this
+}
 
-inline fun Int.ifNegative(block: () -> Int): Int = if (this < 0) block() else this
+@OptIn(ExperimentalContracts::class)
+inline fun Int.ifNegative(block: () -> Int): Int {
+    contract {
+        callsInPlace(block, InvocationKind.AT_MOST_ONCE)
+    }
+    return if (this < 0) block() else this
+}
