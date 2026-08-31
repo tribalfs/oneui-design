25e7e6b399d929605c215ef964c302aa34114015
tribalfs <tribalfs@gmail.com>
2026-08-31 14:39:10 +0800

    lib: bump to 0.9.19+oneui8

--- buildSrc/src/main/kotlin/com/oneui/Versions.kt
+++ buildSrc/src/main/kotlin/com/oneui/Versions.kt
@@ -2,7 +2,7 @@ package com.oneui
 
 object Versions {
     val metadata = mapOf(
-        "oneui-design" to listOf("0.9.18+oneui8", 24, 37),
+        "oneui-design" to listOf("0.9.19+oneui8", 24, 37),
         "stub" to listOf("1.0.0", 24, 37),
         "sample-app" to listOf("0.9.4+oneui8", 24, 37)
     )
